fd2e74e253c87084d681921d950c7d8274031f2e
Nimra Rehman <nimra.rehman.42@gmail.com>
2021-03-02 16:01:08 +0100

    code upadted

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QDebug>
+#include <QDateTime>
+#include <QDate>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->horizontalSlider, &QSlider::valueChanged,ui->progressBar, &QProgressBar::setValue);
+    QPixmap pix("//home/nimra/Cplusplus/guiApplication/welcome.png");
+    ui -> label_image->setPixmap(pix);
+    
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()),this, SLOT(myfunction()));
+    timer -> start(1000);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::myfunction()
+{
+    QTime time = QTime::currentTime();
+    //QDate date = QDate ::currentDate();
+
+    QString time_text = time.toString("hh : mm : ss ");
+    ui -> label_date_time_2 ->setText(time_text);
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 #include "secondwindow.h"
 
 QT_BEGIN_NAMESPACE
@@ -15,12 +16,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void myfunction();
 
 private slots:
     void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
     secondWindow *secondwindow;//object
 
 };
